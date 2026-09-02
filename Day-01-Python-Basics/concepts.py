8deba9159de5b4dd5feb758bbe8a92c483c9e5b3
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-02 19:29:46 +0530

    Add Day 1 Python basics concepts

---
+++ Day-01-Python-Basics/concepts.py
@@ -0,0 +1,12 @@
+# Day 1 - Python Basics
+
+# Using print()
+print("Hello, World!")
+print("I am learning Python.")
+print("My goal is to become a Data Scientist.")
+
+
+# Comments
+# This is a single-line comment.
+
+print("Python is fun to learn!")
