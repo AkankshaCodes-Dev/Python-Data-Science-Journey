7a0f206224d50ba8d78921ccc0838e99a868c972
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-03 19:01:08 +0530

    Add Day 2 practice tasks

---
+++ Day-02-Variables-Data-Types/tasks.py
@@ -0,0 +1,37 @@
+# Day 2 - Variables and Data Types Practice Tasks
+
+# Task 1: Store personal information in variables
+name = "Akanksha"
+age = 20
+goal = "Data Science"
+
+print(name)
+print(age)
+print(goal)
+
+# Task 2: Practice different data types
+student_name = "Akanksha"
+student_age = 20
+percentage = 85.5
+is_student = True
+
+print(student_name)
+print(student_age)
+print(percentage)
+print(is_student)
+
+# Task 3: Check data types
+print(type(student_name))
+print(type(student_age))
+print(type(percentage))
+print(type(is_student))
+
+# Task 4: Use variables in calculations
+a = 10
+b = 5
+
+addition = a + b
+multiplication = a * b
+
+print(addition)
+print(multiplication)
