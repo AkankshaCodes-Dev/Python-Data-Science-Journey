876f6f1a08e985beaa2a8aff0356e70908ee16e3
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-03 18:59:55 +0530

    Add Day 2 concepts

---
+++ Day-02-Variables-Data-Types/concepts.py
@@ -0,0 +1,28 @@
+# Day 2 - Variables and Data Types
+
+# Variables
+name = "Akanksha"
+age = 20
+height = 5.0
+is_learning = True
+
+print(name)
+print(age)
+print(height)
+print(is_learning)
+
+
+# Checking data types using type()
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
+
+
+# Assignment operator
+x = 10
+y = 20
+
+sum = x + y
+
+print(sum)
