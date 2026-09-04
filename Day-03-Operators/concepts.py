653720a946495cb9d03b9bc48b866059626f26f4
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-04 18:18:57 +0530

    Add Day 3 operators concepts

---
+++ Day-03-Operators/concepts.py
@@ -0,0 +1,37 @@
+# Day 3 - Operators
+
+# Arithmetic Operators
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a // b)  # Floor Division
+print(a % b)   # Modulus
+print(a ** b)  # Exponent
+
+
+# Assignment Operator
+x = 10
+x = x + 5
+
+print(x)
+
+
+# Comparison Operators
+print(10 == 10)
+print(10 != 5)
+print(10 > 5)
+print(10 < 5)
+print(10 >= 10)
+print(5 <= 10)
+
+
+# Logical Operators
+age = 20
+
+print(age >= 18 and age <= 25)
+print(age < 18 or age > 60)
+print(not(age >= 18))
