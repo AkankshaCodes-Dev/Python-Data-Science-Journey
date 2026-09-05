7c37b05ac5ea2fa4d74e20858a0bbb100d7b74a9
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-05 18:56:37 +0530

    Add Day 4 input and type conversion concepts

---
+++ Day-04-Input-Type-Conversion/concepts.py
@@ -0,0 +1,35 @@
+# Day 4 - Input and Type Conversion
+
+# Taking input from the user
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+
+# input() returns a string
+age = input("Enter your age: ")
+
+print(age)
+print(type(age))
+
+
+# Converting string to integer
+age = int(age)
+
+print(age)
+print(type(age))
+
+
+# Converting string to float
+height = float(input("Enter your height: "))
+
+print(height)
+print(type(height))
+
+
+# Converting a number to string
+number = 100
+text = str(number)
+
+print(text)
+print(type(text))
