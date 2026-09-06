d05b88d22ff7b37e8fb5b1482a92c429f33b4cf5
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-06 19:18:51 +0530

    Add Day 5 if statement concepts

---
+++ Day-05-if-Statement/concepts.py
@@ -0,0 +1,28 @@
+# Day 5 - if Statement
+
+# Basic if statement
+age = 20
+
+if age >= 18:
+    print("You are an adult.")
+
+
+# Checking a number
+number = 10
+
+if number > 0:
+    print("The number is positive.")
+
+
+# Using equality
+name = "Akanksha"
+
+if name == "Akanksha":
+    print("Name matched.")
+
+
+# if with user input
+marks = int(input("Enter your marks: "))
+
+if marks >= 40:
+    print("You passed.")
