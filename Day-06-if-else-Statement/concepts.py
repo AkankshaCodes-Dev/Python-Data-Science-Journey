94624ac259135bcb1fd0cf2daed0287bf35dde48
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-07 19:27:33 +0530

    Add Day 6 if-else concepts

---
+++ Day-06-if-else-Statement/concepts.py
@@ -0,0 +1,36 @@
+# Day 6 - if-else Statement
+
+# Basic if-else
+age = 20
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+# Checking whether a number is positive or not
+number = 10
+
+if number > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
+
+
+# Checking even or odd
+number = 7
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
+# if-else with user input
+marks = int(input("Enter your marks: "))
+
+if marks >= 40:
+    print("You passed.")
+else:
+    print("You failed.")
