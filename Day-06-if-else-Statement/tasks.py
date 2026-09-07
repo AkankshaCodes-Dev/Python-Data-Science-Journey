d9a06a56895d207a2f60ca91e4b14d2b3a913bb7
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-07 19:28:11 +0530

    Add Day 6 if-else practice tasks

---
+++ Day-06-if-else-Statement/tasks.py
@@ -0,0 +1,45 @@
+# Day 6 - if-else Statement Practice Tasks
+
+# Task 1: Check whether a number is positive or negative
+number = -5
+
+if number >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
+
+# Task 2: Check whether a person can vote
+age = 16
+
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You cannot vote yet.")
+
+
+# Task 3: Check whether a number is even or odd
+number = 12
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
+# Task 4: Check whether marks are passing
+marks = 35
+
+if marks >= 40:
+    print("You passed.")
+else:
+    print("You failed.")
+
+
+# Task 5: Take a number from the user
+num = int(input("Enter a number: "))
+
+if num > 50:
+    print("The number is greater than 50.")
+else:
+    print("The number is 50 or less.")
