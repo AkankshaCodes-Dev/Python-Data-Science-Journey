8d8e71e21653cf031cc8af46d5b33354a6abab3e
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-09 20:18:06 +0530

    Add Day 8 for loop concepts

---
+++ Day-08-for-Loops/concepts.py
@@ -0,0 +1,25 @@
+# Day 8 - for Loops
+
+
+# Example 1: Print numbers 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+
+# Example 2: Print even numbers 1 to 20
+
+for i in range(2, 21, 2):
+    print(i)
+
+
+# Example 3: Multiplication table of 5
+
+for i in range(1, 11):
+    print(5 * i)
+
+
+# Example 4: Print name 5 times
+
+for i in range(5):
+    print("Akanksha")
