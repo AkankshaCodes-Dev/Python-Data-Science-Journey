1603afa56521577a5c1509bb06b7df1472a3854f
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-11 18:32:55 +0530

    Add Day 10 while loop practice tasks

---
+++ Day-10-while-Loops/tasks.py
@@ -0,0 +1,49 @@
+# Day 10 - while Loop Practice Tasks
+
+
+# Task 1: Print numbers from 1 to 10
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+# Task 2: Print even numbers from 1 to 20
+
+i = 2
+
+while i <= 20:
+    print(i)
+    i = i + 2
+
+
+# Task 3: Reverse counting from 10 to 1
+
+i = 10
+
+while i >= 1:
+    print(i)
+    i = i - 1
+
+
+# Task 4: Print multiplication table of 5
+
+i = 1
+
+while i <= 10:
+    print(5 * i)
+    i = i + 1
+
+
+# Task 5: Sum numbers from 1 to 10
+
+i = 1
+total = 0
+
+while i <= 10:
+    total = total + i
+    i = i + 1
+
+print("Sum:", total)
