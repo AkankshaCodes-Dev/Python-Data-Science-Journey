73e3a41fe51b40501dabf69c3d79bf755f4f90a5
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-13 19:46:47 +0530

    Add Day 12 string practice tasks

---
+++ Day-12-Strings/tasks.py
@@ -0,0 +1,34 @@
+# Day 12 - Strings Practice Tasks
+
+
+# Task 1: Create a string containing your name
+
+name = "Akanksha"
+
+print(name)
+
+
+# Task 2: Print the first character of your name
+
+print(name[0])
+
+
+# Task 3: Print the last character of your name
+
+print(name[-1])
+
+
+# Task 4: Print the first three characters of your name
+
+print(name[0:3])
+
+
+# Task 5: Find the length of your name
+
+print(len(name))
+
+
+# Task 6: Print each character of your name using a for loop
+
+for character in name:
+    print(character)
