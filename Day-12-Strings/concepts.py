107eef6b080be7e3374a14900288efb2d28b31fa
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-13 19:46:13 +0530

    Add Day 12 string concepts

---
+++ Day-12-Strings/concepts.py
@@ -0,0 +1,56 @@
+# Day 12 - Strings
+
+
+# Example 1: Creating a string
+
+name = "Akanksha"
+
+print(name)
+
+
+# Example 2: Single quotes
+
+city = 'Ballari'
+
+print(city)
+
+
+# Example 3: String indexing
+
+word = "Python"
+
+print(word[0])
+print(word[1])
+print(word[2])
+
+
+# Example 4: Negative indexing
+
+word = "Python"
+
+print(word[-1])
+print(word[-2])
+print(word[-3])
+
+
+# Example 5: String slicing
+
+word = "Python"
+
+print(word[0:3])
+print(word[2:6])
+
+
+# Example 6: String length
+
+word = "Python"
+
+print(len(word))
+
+
+# Example 7: Accessing characters
+
+name = "Akanksha"
+
+for character in name:
+    print(character)
