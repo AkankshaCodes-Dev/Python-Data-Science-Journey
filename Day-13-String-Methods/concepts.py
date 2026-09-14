d048394c8d50bfa3e8238465d388e474f2cb1604
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-14 14:56:36 +0530

    Add Day 13 string methods concepts

---
+++ Day-13-String-Methods/concepts.py
@@ -0,0 +1,52 @@
+# Day 13 - String Methods
+
+
+# Example 1: lower()
+
+text = "PYTHON"
+
+print(text.lower())
+
+
+# Example 2: upper()
+
+text = "python"
+
+print(text.upper())
+
+
+# Example 3: strip()
+
+text = "   Hello Python   "
+
+print(text.strip())
+
+
+# Example 4: replace()
+
+text = "I like Java"
+
+print(text.replace("Java", "Python"))
+
+
+# Example 5: find()
+
+text = "Hello Python"
+
+print(text.find("Python"))
+
+
+# Example 6: count()
+
+text = "banana"
+
+print(text.count("a"))
+
+
+# Example 7: split()
+
+text = "Python is easy"
+
+words = text.split()
+
+print(words)
