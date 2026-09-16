11967b63e19317ea9c8b5078e8f2e802ed3f0ab0
AkankshaCodes-Dev <akanksha.bodimi@gmail.com>
2026-09-16 20:41:45 +0530

    Add Day 15 list basics practice tasks

---
+++ Day-15-Lists-Basics/tasks.py
@@ -0,0 +1,36 @@
+# Day 15 - Lists Basics Practice Tasks
+
+
+# Task 1: Create a student marks list
+
+marks = [85, 90, 78, 92, 88]
+
+print(marks)
+
+
+# Task 2: Print the first item
+
+print(marks[0])
+
+
+# Task 3: Print the last item
+
+print(marks[-1])
+
+
+# Task 4: Change one element
+
+marks[2] = 80
+
+print(marks)
+
+
+# Task 5: Print all elements one by one
+
+for mark in marks:
+    print(mark)
+
+
+# Task 6: Find the length of the list
+
+print(len(marks))
